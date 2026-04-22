48a2672217be987a23a9e723f94c19584b6722b2
C. Thang Nguyen <thangckt@gmail.com>
2026-04-22 15:57:36 +0900

    update logo

--- asext/__init__.py
+++ asext/__init__.py
@@ -1,6 +1,6 @@
 """ASEXT: Python package extends functions of ASE (Atomic Simulation Environment).
 
-<img src="./_assets/image/kde_color_128x128.png" style="float: left; margin-right: 20px" width="120"/>
+<img src="./_assets/image/logo_alff.svg" style="float: left; margin-right: 20px" width="120"/>
 
 Developed by [C.Thang Nguyen](https://thangckt.github.io)
 """
